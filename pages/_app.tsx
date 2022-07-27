e8575d9fb599ec58c029fe42c02ab05c7ec981d0
maybemaby <bym9749@gmail.com>
2022-07-26 19:51:08 -0700

    Initialize styled-components config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,33 @@
 import type { AppProps } from "next/app";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import { lightTheme } from "../theme";
+
+const GlobalStyle = createGlobalStyle`
+html,
+body {
+  padding: 0;
+  margin: 0;
+  font-family: Fira Sans ,-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+}
+
+* {
+  box-sizing: border-box;
+}
+div#__next {
+  min-height: 100vh;
+}
+`;
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={lightTheme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default MyApp;
