a094b98e27b9896bb5196ed7956c0acd58e9c927
maybemaby <bym9749@gmail.com>
2022-07-31 22:13:19 -0700

    Handle strings or plain js obj in PostVideo controller

--- lib/features/video/controllers/PostVideo.ts
+++ lib/features/video/controllers/PostVideo.ts
@@ -3,7 +3,12 @@ import { postVideoSchema, PostVideoDto } from "../models/Video";
 import VideoService from "../VideoService";
 
 export const PostVideo = async (req: NextApiRequest, res: NextApiResponse) => {
-  const dto = JSON.parse(req.body) as PostVideoDto;
+  let dto: PostVideoDto;
+  if (typeof req.body === "string") {
+    dto = JSON.parse(req.body) as PostVideoDto;
+  } else {
+    dto = req.body as PostVideoDto;
+  }
   if (await postVideoSchema.isValid(dto)) {
     const video = await VideoService.createVideo(dto);
     res.status(201).json(video);
