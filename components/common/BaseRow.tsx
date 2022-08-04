7ae9142dd4190af7d1b729529059078b36362d53
maybemaby <bym9749@gmail.com>
2022-08-03 22:27:52 -0700

    Add props for baserow

--- components/common/BaseRow.tsx
+++ components/common/BaseRow.tsx
@@ -1,9 +1,13 @@
 import styled from "styled-components";
 import { CommonStyle } from "../../lib/types/CommonStyle";
 
-const BaseRow = styled.div<CommonStyle>`
+const BaseRow = styled.div<
+  CommonStyle & { justify?: "space-between" | "flex-end" | "space-evenly" }
+>`
   display: flex;
   flex-direction: row;
+  align-items: center;
+  justify-content: ${(props) => props.justify ?? "flex-start"};
   padding: ${(props) => props.padding};
   margin: ${(props) => props.margin};
   width: ${(props) => props.width};
