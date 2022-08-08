cd2dcf33d2e3fac4d5a180ff550b417a0d62e85c
maybemaby <bym9749@gmail.com>
2022-08-07 17:30:48 -0700

    Add cors headers to api router using config

--- next.config.js
+++ next.config.js
@@ -7,6 +7,29 @@ const nextConfig = {
   images: {
     domains: ["i.ytimg.com"],
   },
+  async headers() {
+    return [
+      {
+        source: "/api/trpc/:path*",
+        headers: [
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,HEAD,POST,PUT,DELETE,PATCH",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: process.env.NEXT_PUBLIC_VERCEL_URL ?? "localhost:3000",
+          },
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
