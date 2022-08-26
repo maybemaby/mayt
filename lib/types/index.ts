f88de6323bb2b0ae727b174b66be4349e735591f
maybemaby <bym9749@gmail.com>
2022-08-25 20:14:57 -0700

    Add FindVideoReturn trpc query videos.find infer

--- lib/types/index.ts
+++ lib/types/index.ts
@@ -1,4 +1,4 @@
-import { VideoLike } from "./VideoLike";
+import { VideoLike, VideoFindReturn } from "./VideoLike";
 import { PlaylistLike } from "./PlaylistLike";
 
-export type { VideoLike, PlaylistLike };
+export type { VideoLike, PlaylistLike, VideoFindReturn };
