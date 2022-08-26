764af909d78db174084a9e9fcb4862f43f1e0a69
maybemaby <bym9749@gmail.com>
2022-08-25 20:16:09 -0700

    Create Badge component for chiplike items

---
+++ components/common/Badge.tsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+
+interface BadgeProps {
+  fill?: string;
+  stroke?: string;
+}
+
+const Badge = styled.div<BadgeProps>`
+  display: flex;
+  justify-content: center;
+  border: 2px solid ${(props) => props.stroke ?? props.theme.color.primary[300]};
+  background: ${(props) => props.fill ?? "transparent"};
+  color: ${(props) => props.stroke ?? props.theme.color.primary[500]};
+  border-radius: 20px;
+  min-height: 20px;
+  min-width: 50px;
+  padding: 5px;
+  transition: background-color 300ms ease;
+
+  &:hover {
+    background: ${(props) => props.theme.color.grey[50]};
+  }
+`;
+
+export const ButtonBadge = styled.button<BadgeProps>`
+  font-size: 1rem;
+  font-family: "Nunito", Oxygen, sans-serif;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 2px solid ${(props) => props.stroke ?? props.theme.color.primary[300]};
+  background: ${(props) => props.fill ?? "transparent"};
+  color: ${(props) => props.stroke ?? props.theme.color.primary[500]};
+  border-radius: 20px;
+  min-height: 20px;
+  min-width: 50px;
+  padding: 5px 10px;
+  transition: background-color 300ms ease;
+
+  &:hover {
+    background: ${(props) => props.theme.color.grey[50]};
+    cursor: pointer;
+  }
+`;
+
+export default Badge;
